3adfbe6cb2869d0c684723726d91a3d61cd57888
thorsummoner <thorsummoner@live.com>
2014-02-09 17:29:00 -0800

    working on implementing svg elements ref http://codepen.io/anon/pen/ADiuv

---
+++ CREDITS.md
@@ -0,0 +1,3 @@
+| Author | Asset |
+| :--- | :--- |
+| [Emper24](http://emper24.deviantart.com/) | [Equestria's Moon](http://emper24.deviantart.com/art/Equestria-s-Moon-302308051) |
--- js/Svg.js
+++ js/Svg.js
@@ -1,13 +1,19 @@
 var Svg = function(id, parent, xmlns) {
-	this.xmlns = xmlns ? xmlns : "http://www.w3.org/2000/svg"
+	var xmlns  = xmlns ? xmlns : "http://www.w3.org/2000/svg"
 	var parent = parent ? document.querySelector(parent) : document.body
 
-	this.node = document.createElementNS(this.xmlns, 'svg')
-	this.node.id = id ? id : uuid()
+	node = document.createElementNS(xmlns, 'svg')
+	node.xmlns = xmlns
+	node.id = id ? id : uuid()
+	node.style.display = "block";
 
-	parent.appendChild(this.node)
+	parent.appendChild(node)
 
-}
-Svg.prototype.maximize = function () {
-	log(this.node.style.backgroundColor = 'red')
+	node.maximize = function () {
+		this.setAttributeNS (null, "viewBox", "0 0 " + this.parentNode.scrollWidth + " " + this.parentNode.scrollHeight);
+		this.setAttributeNS(null, "width", this.parentNode.scrollWidth);
+		this.setAttributeNS(null, "height", this.parentNode.scrollHeight);
+	}
+
+	return node
 }
